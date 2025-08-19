af2cf5e1412db6abc49865e595be2cc8a322ccfa
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2025-08-19 15:52:55 +0400

    feat(users): follow

--- src/modules/users/__init__.py
+++ src/modules/users/__init__.py
@@ -1,5 +1,6 @@
 from src.modules.users.controllers import UserController
 
+from .models import FollowModel as FollowModel
 from .models import UserModel as UserModel
 
 controllers = (UserController,)
--- src/modules/users/models.py
+++ src/modules/users/models.py
@@ -1,6 +1,8 @@
 from typing import TYPE_CHECKING, List
+from uuid import UUID
 
 from advanced_alchemy.base import UUIDAuditBase
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
@@ -17,3 +19,25 @@ class UserModel(UUIDAuditBase):
     listenings: Mapped[List["ListeningModel"]] = relationship(
         "ListeningModel", back_populates="user"
     )
+
+    following: Mapped[List["FollowModel"]] = relationship(
+        "FollowModel", back_populates="from_user"
+    )
+    followers: Mapped[List["FollowModel"]] = relationship(
+        "FollowModel", back_populates="to_user"
+    )
+
+
+class FollowModel(UUIDAuditBase):
+    __tablename__ = "follows"
+    __table_args__ = (UniqueConstraint("from_user_id", "to_user_id", name="uq_follow"),)
+
+    from_user_id: Mapped[UUID] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), index=True, nullable=False
+    )
+    to_user_id: Mapped[UUID] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), index=True, nullable=False
+    )
+
+    from_user: Mapped["UserModel"] = relationship("UserModel", back_populates="singers")
+    to_user: Mapped["UserModel"] = relationship("UserModel", back_populates="singers")
