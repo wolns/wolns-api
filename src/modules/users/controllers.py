65f385db3c835a05185a2dc64256c7bb9b9a0882
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2025-09-10 14:32:14 +0300

    feat(controllers): auth

--- src/modules/users/controllers.py
+++ src/modules/users/controllers.py
@@ -1,10 +1,14 @@
+from typing import Any
 from uuid import UUID
 
 from dishka import FromDishka
-from litestar import Controller, Response, delete, get, post
+from litestar import Controller, Request, Response, delete, get, post
+from litestar.security.jwt import Token
 
 from src.core.security import jwt_auth
+from src.modules.tracks.dto import ITracksUser
 from src.modules.users.dto import (
+    ISingersUser,
     IUserBase,
     IUserRead,
     IUserSignIn,
@@ -41,16 +45,49 @@ class UserController(Controller):
         )
 
     @post("/me")
-    async def get_me(self, user: UserModel) -> IUserBase: ...
+    async def get_me(
+        self,
+        request: Request[UserModel, Token, Any],
+        user_service: FromDishka[UserService],
+    ) -> IUserBase:
+        user_dto = user_service.to_schema(request.user, schema_type=IUserBase)
+        return user_dto
 
     @get("/{id:uuid}")
-    async def get_by_id(self, id: UUID, user: UserModel) -> IUserRead: ...
+    async def get_by_id(
+        self,
+        id: UUID,
+        request: Request[UserModel, Token, Any],
+    ) -> IUserRead: ...
 
     @post("/{id:uuid}/follow")
-    async def follow(self, id: UUID, user: UserModel) -> None: ...
+    async def follow(
+        self,
+        id: UUID,
+        request: Request[UserModel, Token, Any],
+    ) -> None: ...
 
     @delete("/{id:uuid}/follow")
-    async def unfollow(self, id: UUID, user: UserModel) -> None: ...
+    async def unfollow(
+        self,
+        id: UUID,
+        request: Request[UserModel, Token, Any],
+    ) -> None: ...
 
     @get("/{id:uuid}/singers/stats")
-    async def get_singers_stats(self, id: UUID, user: UserModel) -> IUserRead: ...
+    async def get_singers_stats(
+        self,
+        id: UUID,
+        request: Request[UserModel, Token, Any],
+        page: int = 1,
+        page_size: int = 20,
+    ) -> ISingersUser: ...
+
+    @get("/{id:uuid}/tracks")
+    async def get_tracks(
+        self,
+        id: UUID,
+        request: Request[UserModel, Token, Any],
+        page: int = 1,
+        page_size: int = 20,
+    ) -> ITracksUser: ...
